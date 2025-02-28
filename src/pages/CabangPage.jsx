6753a413bf3d0cf3073eb19f2fe415bd30ff5f04
GendisPutri <febrianybulan14@gmai.com>
2025-02-28 09:06:35 +0700

    Memperbarui Konflik Setelah Pull

--- src/pages/CabangPage.jsx
+++ src/pages/CabangPage.jsx
@@ -36,7 +36,7 @@ const HomePage = () => {
     <div className="cabangpage">
       <header className="w-100 min-vh-100 d-flex align-items-center hero-section">
         <Container>
-          <Row className="d-flex align-items-center">
+          <Row className="d-flex align-items-left">
             <Col lg={6}>
               <h1 className="title">Cabang</h1>
               <p className="description">
@@ -56,17 +56,21 @@ const HomePage = () => {
             {cabangData.map((cabang, index) => (
               <Col md={3} key={index} className="mb-4">
                 <Card className="h-100 shadow-sm card-hover">
-                  <Card.Header className="fw-bold">{cabang.title}</Card.Header>
-                  <Card.Body>
-                    <p>
-                      <i className="bi bi-house-door-fill"></i> {cabang.lokasi}
-                    </p>
-                    <p>
-                      <i className="bi bi-whatsapp"></i> <a href="https://api.whatsapp.com/send/?phone=62816343742&text&type=phone_number&app_absent=0">{cabang.linkWa}</a>
-                    </p>
-                    <p>
-                      <i className="bi bi-instagram"></i> <a href="https://www.instagram.com/campusdigital.id">{cabang.linkIg}</a>
-                    </p>
+                  <Card.Header className="fw-bold text-center">{cabang.title}</Card.Header>
+                  <Card.Body className="d-flex flex-column gap-2">
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-house-door-fill me-2"></i> {cabang.lokasi}
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-whatsapp me-2"></i>
+                      <a href="https://api.whatsapp.com/send/?phone=62816343742&text&type=phone_number&app_absent=0">
+                        {cabang.linkWa}
+                      </a>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-instagram me-2"></i>
+                      <a href="https://www.instagram.com/campusdigital.id">{cabang.linkIg}</a>
+                    </div>
                   </Card.Body>
                 </Card>
               </Col>
